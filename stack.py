ef149814575a5c1b54c39f342d82e7a7aa7c1c99
Zenara Daley <zrdaley@gmail.com>
2018-11-18 13:48:23 -0500

    add stack

---
+++ stack.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+class Node:
+  def __init__(self, val):
+    self.value = val
+    self.next = None
+
+class Stack:
+  def __init__(self, values):
+    if len(values) == 0:
+      self.top = None
+      return
+  
+    self.top = Node(values[-1])
+    prev = self.top
+    for val in values[1::-1]:
+      node = Node(val)
+      prev.next = node
+      prev = node
+    
+  
+  def pop(self):
+    temp = self.top
+    if self.top is None:
+      return None
+    self.top = self.top.next
+    return temp
+  
+  def push(self, val):
+    new = Node(val)
+    new.next = self.top
+    self.top = new
+
+
+def main():
+  values = raw_input("Please enter values to be turned into a stack:\n").split()
+  stack = Stack(values)
+
+  stack.push("q")
+
+  v = stack.pop()
+  while v is not None:
+    print v.value
+    v = stack.pop()
+
+if __name__ == "__main__":
+   main()
